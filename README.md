185a455d875c420b6ac3f87fb44f9b2d58da595b
Alexandru Rosianu <me@aluxian.com>
2017-09-21 09:45:19 +0200

    Update README, move env vars from fishd to config

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 ## Dependencies
 
 ```bash
-~> brew install fzf
-~> brew install the_silver_searcher
-~> brew install thefuck
+~> brew install fzf nvim the_silver_searcher thefuck
 ~> brew install --HEAD https://raw.githubusercontent.com/junegunn/blsd/master/blsd.rb
 ```
--- config.fish
+++ config.fish
@@ -1,2 +1,15 @@
 # load virtualfish
 eval (python -m virtualfish)
+
+# load secret env vars
+source ~/.config/fish/secrets.fish
+
+# general env config
+set -x EDITOR 'nvim'
+
+# fzf env config
+set -x FZF_ALT_C_COMMAND 'blsd'
+set -x FZF_ALT_C_OPTS '--preview \'tree -C {} | head -200\''
+set -x FZF_CTRL_T_COMMAND ag '--hidden --ignore .git -g ""'
+set -x FZF_DEFAULT_COMMAND ag '--hidden --ignore .git -g ""'
+set -x FZF_DEFAULT_OPTS '--no-height --no-reverse'
