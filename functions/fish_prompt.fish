d38990d4488e2ab799c728c44b692d1ccd9e41a4
Alexandru Rosianu <me@aluxian.com>
2018-03-17 12:59:24 +0000

    change left prompt order

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -1,8 +1,14 @@
 function fish_prompt
-  echo -s (set_color $fish_color_cwd) (prompt_pwd) \
+  set -l last_status $status
+  set -l last_status_color "green"
+  if test $last_status -ne 0
+    set last_status_color "red"
+  end
+
+  echo -s \
+    (set_color $last_status_color) $last_status \
     (set_color yellow) (__fish_git_prompt) \
     (set_color blue) (__kube_prompt) \
-    (set_color normal) \
-    \n (set_color $fish_color_cwd) "~" \
+    (set_color $fish_color_cwd) " " (prompt_pwd) \
     (set_color normal) "> "
 end
