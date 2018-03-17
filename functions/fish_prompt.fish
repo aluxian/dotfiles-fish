da0f6ac8b0e5f54cf6668a16588e3146a0e8a3ba
Alexandru Rosianu <me@aluxian.com>
2018-03-17 12:48:07 +0000

    merge fish_prompt into 1 echo

--- functions/fish_prompt.fish
+++ functions/fish_prompt.fish
@@ -2,6 +2,7 @@ function fish_prompt
   echo -s (set_color $fish_color_cwd) (prompt_pwd) \
     (set_color yellow) (__fish_git_prompt) \
     (set_color blue) (__kube_prompt) \
-    (set_color normal)
-  echo -s (set_color $fish_color_cwd) "~" (set_color normal) "> "
+    (set_color normal) \
+    \n (set_color $fish_color_cwd) "~" \
+    (set_color normal) "> "
 end
