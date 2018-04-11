3a7a4415977bd617505ed5f907c454292d577b20
Alexandru Rosianu <me@aluxian.com>
2018-04-11 18:47:56 +0100

    sync

--- config.fish
+++ config.fish
@@ -13,3 +13,10 @@ set -x FZF_ALT_C_OPTS '--preview \'tree -C {} | head -200\''
 set -x FZF_CTRL_T_COMMAND ag '--hidden --ignore .git -g ""'
 set -x FZF_DEFAULT_COMMAND ag '--hidden --ignore .git -g ""'
 set -x FZF_DEFAULT_OPTS '--no-height --no-reverse'
+
+# go lang
+set -x GOPATH $HOME/.go
+set -x PATH $GOPATH/bin $PATH
+
+# iOS fastlane
+set -x PATH $HOME/.fastlane/bin $PATH
